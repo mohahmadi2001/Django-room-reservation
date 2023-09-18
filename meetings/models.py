8cc5ff5976419c374899bdfb871b8f06535a6b41
Mohammad Mahdi <moh.ahmadi2001@yahoo.com>
2023-09-18 23:35:34 +0330

    meetings basic model

--- meetings/models.py
+++ meetings/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class MeetingRoom(models.Model):
+    STATUS_CHOICES = (
+        ('empty', 'empty'),
+        ('full', 'full'),
+    )
+    name = models.CharField(_("name"), max_length=50)
+    status = models.CharField(_("status"), max_length=10,choices=STATUS_CHOICES,default="empty")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = _("MeetingRoom")
+        verbose_name_plural = _("MeetingRooms")
+
+    def __str__(self):
+        return self.name
+
