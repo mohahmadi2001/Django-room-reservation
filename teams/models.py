f04f7b20cbe08059aa93ff09beead928abe4d391
Mohammad Mahdi <moh.ahmadi2001@yahoo.com>
2023-09-19 12:06:30 +0330

    team model

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(_("name"), max_length=50)
+    members = models.ManyToManyField(
+        "users.CustomUser",
+        verbose_name=_("member"),
+        blank=True,
+        related_name="team_members"
+    )
+    
+
+    class Meta:
+        verbose_name = _("Team")
+        verbose_name_plural = _("Teams")
+
+    def __str__(self):
+        return self.name
+
+
