cd2c710bb1b4955702a81c77b5c340bc7f06519b
Mohammad Mahdi <moh.ahmadi2001@yahoo.com>
2023-09-25 11:22:59 +0330

    change team for user with foreign key

--- users/models.py
+++ users/models.py
@@ -8,13 +8,12 @@ from django.utils.translation import gettext_lazy as _
 class CustomUser(AbstractUser,AbstractBaseModel):
     is_email_confirmed = models.BooleanField(_("email confirmed"),default=False)
     is_team_manager = models.BooleanField(_("team manager"),default=False)
-    team = models.OneToOneField(
+    team = models.ForeignKey(
         "teams.Team",  
         verbose_name=_("team"),
         null=True,  
         blank=True,
         on_delete=models.SET_NULL,
-        unique=True
     )
 
     class Meta:
